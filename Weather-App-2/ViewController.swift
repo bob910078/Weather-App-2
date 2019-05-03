46cd1d6feb99fcee3c1f9f787f08790c964e7a92
Bob Change <bob910078@gmail.com>
2019-05-03 19:06:13 +0800

    Take off useless method.

--- Weather-App-2/ViewController.swift
+++ Weather-App-2/ViewController.swift
@@ -61,11 +61,6 @@ class ViewController: UIViewController,
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     private func initialLayout() {
         self.descriptionLabel.isHidden = true
